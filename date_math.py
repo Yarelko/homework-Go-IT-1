bf535317a4f7b978deba616191b2186fb647a3aa
Yarelko <m.bro.yarik@gmail.com>
2025-12-19 13:26:40 +0200

    Update date_math.py

--- date_math.py
+++ date_math.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 
 def get_days_from_today(date_str):
-    given_date = datetime.strptime(date_str, "%Y-%m-%d").date()
-    
-    today = datetime.today().date()
-    
-    delta = today - given_date
-    
-    return delta.days
-print(get_days_from_today("2020-10-09"))
+    try:
+        given_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        return (today - given_date).days
+    except ValueError:
+        return None
